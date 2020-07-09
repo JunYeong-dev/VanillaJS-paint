82e5f58bc9fffc05f2795f6085d2177cc7ac0391
Jun-cloud <kjy8827@gmail.com>
2020-07-09 18:24:38 +0900

    Add comments

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+// canvas는 두개의 사이즈를 정의 해 줘야함 
+// 1. css의 canvas사이즈 - css
+// 2. element(픽셀)를 다루기 위한 사이즈 - js
+
 canvas.width = 700;
 canvas.height = 700;
 
+// line의 색상과 굵기 설정
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
