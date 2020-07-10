ec81da51f6d3ef468329444505912beedca4927c
Jun-cloud <kjy8827@gmail.com>
2020-07-10 18:25:10 +0900

    Create function - change line size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+
 
 // canvas는 두개의 사이즈를 정의 해 줘야함 
 // 1. css의 canvas사이즈 - css
@@ -41,6 +43,11 @@ function handleColorClick(event){
     ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -51,3 +58,7 @@ if(canvas){
 Array.from(colors).forEach(
     color => color.addEventListener("click", handleColorClick)
 );
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
