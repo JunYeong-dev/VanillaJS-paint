cd0037aee0515a1ba9890ec38cf0000dffd3cba2
Jun-cloud <kjy8827@gmail.com>
2020-07-12 18:06:38 +0900

    Create function - fill canvas

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = "700";
 
 // canvas는 두개의 사이즈를 정의 해 줘야함 
 // 1. css의 canvas사이즈 - css
 // 2. element(픽셀)를 다루기 위한 사이즈 - js
 
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
 // line의 색상과 굵기 설정
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -43,6 +46,7 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor
     ctx.strokeStyle = color;
+    ctx.fillStyle = ctx.strokeStyle;
 }
 
 function handleRangeChange(event){
@@ -60,11 +64,20 @@ function handleModeClick(){
     }
 }
 
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    } else {
+
+    }
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick)
 }
 
 Array.from(colors).forEach(
